974d2675690f4c6eb8829dbc76c8776bec22a908
Masian Parkire <parkiremasian@gmail.com>
2023-03-02 08:26:28 +0300

    made changes

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -3,7 +3,8 @@ var sian = Human("Masian",42,67)
 sian.eat(4)
     println(sian.weight)
     sian.speak("My name is Mercy and I love kotlin")
-    sian.birthday(1)
+    sian.birthday()
+    println(sian.age)
    var mine = user("Mercy","Masian","masianmercy@gmail.com","0734567523","masi@@23")
 println(mine.email)
     println(mine.lastName)
@@ -17,9 +18,9 @@ class Human(var name : String, var age : Int,var weight :Int){
     fun speak (speech :String){
         println(speech)
     }
-    fun birthday(add:Int){
+    fun birthday(){
         age += 1
-        println(age)
+
     }
 }
 data class user(var firstName:String,var lastName:String,var email:String, var phoneNumber:String, var passWord:String)
